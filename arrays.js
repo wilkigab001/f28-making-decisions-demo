bac158cc6e9d051c5d7008ef290820e202833111
Gabriel Wilkins <wilkigab001@gmail.com>
2022-08-18 12:50:24 -0600

    What does obi wan have as he flees from mustafar?

--- arrays.js
+++ arrays.js
@@ -3,5 +3,21 @@ let backPack = []
 backPack.push('Jedi Robe')
 backPack.push('lightsaber')
 backPack.push('Crippling sense of failure and guilt')
+backPack.splice(1,1)
+backPack.push('wookie coat')
+
+backPack.pop()
+
+backPack.push('blue milk', 'baby yoda', 'a child named luke', 'comm device', 'high ground', 'blaster')
 
 console.log(backPack)
+
+let obiwanHouse = backPack.splice(3, 2)
+console.log(backPack)
+console.log(obiwanHouse)
+console.log(obiwanHouse.length)
+console.log(backPack.length)
+
+for(i = 0; i < backPack.length; i++){
+    console.log(backPack[i])
+}
